c813e2d821e1168a5e66417da5c0a093a5ab93a5
Mary <marieshix98@gmail.com>
2023-07-24 23:55:10 +0300

    add auth reducer to the store

--- src/redux/services/store.ts
+++ src/redux/services/store.ts
@@ -1,8 +1,10 @@
 import {configureStore} from "@reduxjs/toolkit"
 import { setupListeners } from "@reduxjs/toolkit/dist/query";
+import { authReducer } from "@/redux/features/usersSlice";
 import { userApi } from "./userApi";
 export const store = configureStore({
     reducer:{
+      auth:authReducer,
       [userApi.reducerPath]: userApi.reducer
     },
     middleware: (getDefaultMiddleware) =>getDefaultMiddleware({}).concat([userApi.middleware]),
