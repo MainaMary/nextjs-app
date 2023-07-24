1c4936962886400050cec600dab4a3274b2a0c45
Mary <marieshix98@gmail.com>
2023-07-24 23:58:38 +0300

    define rtk custom hook to fetch user data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
+"use client";
+
 import Image from 'next/image'
+import { useGetAllUsersQuery } from '@/redux/services/userApi'
 
 export default function Home() {
+  const { isLoading, isFetching, data, error } = useGetAllUsersQuery(null);
+  console.log(data)
   return (
   <div>Hello world</div>
   )
