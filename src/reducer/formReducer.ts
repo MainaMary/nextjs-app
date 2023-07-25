f0ff157a1f9f331524f4bd25a252803be8cc6609
Mary <marieshix98@gmail.com>
2023-07-25 18:18:42 +0300

    implement formReducer to handle form actions

---
+++ src/reducer/formReducer.ts
@@ -0,0 +1,34 @@
+import { ActionTypes, initialStateType } from "@/model/types";
+interface IProps {
+    key:string;
+    value:string
+}
+interface Props {
+    type:string;
+    payload: IProps
+}
+
+const initialState = {
+  username:'',
+  email: '',
+  password:''
+}
+export const formReducer = (state:initialStateType, action:Props) => {
+    const {type, payload} = action
+    switch (type) {
+      case ActionTypes.textInput:
+        return {
+          ...state,
+          [payload.key]: payload.value,
+        };
+      case ActionTypes.siginText:
+        return {
+          ...state,
+          [payload.key]: payload.value
+        }
+      case  ActionTypes.reset:
+        return {initialState}
+      default:
+        throw new Error(`Unknown action type: ${type}`);
+    }
+  };
