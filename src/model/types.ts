887172abaeefa67ee477c8258e55a9db23fb08dd
Mary <marieshix98@gmail.com>
2023-07-25 18:21:49 +0300

    define more data types

--- src/model/types.ts
+++ src/model/types.ts
@@ -1,5 +1,48 @@
+export type CounterState = {
+  value: number;
+};
 export type User = {
     id: number;
     name: string;
     email: number;
   };
+  export enum ActionTypes  {
+    textInput = "TEXT_INPUT",
+    siginText = "SIGNIN_TEXT",
+    reset = "RESET_STATE",
+    login="LOG_OUT",
+    logout="LOG_IN"
+ }
+ export  interface initialStateType {
+  username: string,
+  email:string,
+  password:string
+}
+export interface LabelProps {
+  children:string
+}
+export interface Inputprops {
+  onChange: (x:any)=> void;
+  type:string;
+  name: string;
+  placeholder? :string;
+  value?:string;
+  required?:boolean;
+  multiple?: boolean
+
+}
+export interface Posts{
+  userId:string,
+  id:string,
+  title:string,
+  body:string
+
+}
+export interface Photos {
+  albumId:string,
+  id:string,
+  title:string,
+  url:string,
+  thumbnailUrl:string
+ 
+}
