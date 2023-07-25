09f93754b2e21b6bfddef33a3f3d55e6a4d477d7
Mary <marieshix98@gmail.com>
2023-07-25 18:19:25 +0300

    setup side navigation bar

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image"
+export default function Sidebar(){
+    return <div>
+        <aside className="">
+           <Image src="" width={80} height={80} alt="logo"/>
+           <ul>
+           </ul> 
+        </aside>
+    </div>
+}
