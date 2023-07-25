b17b554531596428b684ce680a2550ecf0e401c8
Mary <marieshix98@gmail.com>
2023-07-25 18:23:07 +0300

    add counter reducer in the global store

--- src/redux/services/store.ts
+++ src/redux/services/store.ts
@@ -1,10 +1,12 @@
 import {configureStore} from "@reduxjs/toolkit"
 import { setupListeners } from "@reduxjs/toolkit/dist/query";
 import { authReducer } from "@/redux/features/usersSlice";
+import { counterReducer } from "../features/paginationSlice";
 import { userApi } from "./userApi";
 export const store = configureStore({
     reducer:{
       auth:authReducer,
+      counter: counterReducer,
       [userApi.reducerPath]: userApi.reducer
     },
     middleware: (getDefaultMiddleware) =>getDefaultMiddleware({}).concat([userApi.middleware]),
