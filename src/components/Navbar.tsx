1ea375984f81006dc1d075c81d24797a5dd2131e
Mary <marieshix98@gmail.com>
2023-07-26 23:57:45 +0300

    check current user logged in from local storage

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,10 @@
+"use client";
+import { useEffect, useState } from "react";
 import Link from "next/link";
+
+import { userDetailsKey, initialUserState } from "@/redux/features/usersSlice";
+import { User } from "@/model/types";
+
 const navLinks = [
   {
     label: "Home",
@@ -17,8 +23,14 @@ const navLinks = [
   },
 ];
 export default function NavBar() {
+  const [currentUser, setCurrentUser] = useState(initialUserState);
+  useEffect(() => {
+    setCurrentUser(localStorage.getItem(userDetailsKey) as unknown as User);
+  }, [currentUser]);
+ console.log(Object.keys(currentUser).length > 0)
+ console.log(currentUser.name,'user name')
   return (
-    <nav className="flex h-20 items-center px-12 bg-slate-500 justify-between">
+    <nav className=" top-0 left-0fixed flex w-full h-20 items-center px-12 bg-slate-500 justify-between">
       <Link href="/" className="text-white font-bold">
         BlogsSite
       </Link>
@@ -30,7 +42,15 @@ export default function NavBar() {
         ))}
       </ul>
       <div>
-        <button>Sign in</button>
+        {Object.keys(currentUser).length > 0 ? (
+          <div>
+            <p className="text-color-400">{currentUser.name}</p>
+          </div>
+        ) : (
+          <Link href={"/auth"}>
+            <button>Sign in</button>
+          </Link>
+        )}
       </div>
     </nav>
   );
