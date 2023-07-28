89ce8cec027f75404c84845901e631475d0ec1c0
Mary <marieshix98@gmail.com>
2023-07-28 23:43:47 +0300

    define the login and register user mutations

--- src/components/Signin.tsx
+++ src/components/Signin.tsx
@@ -1,20 +1,26 @@
 import { useReducer, useState, useEffect } from "react"
 import {AiFillEyeInvisible,AiFillEye} from "react-icons/ai"
+import Link from "next/link"
 import { useRouter } from "next/navigation"
+import { toast } from "react-toastify"
 
 
-import { ActionTypes, User} from "@/model/types"
+import { ActionTypes, User, ResponseProp} from "@/model/types"
 import { formReducer } from "@/reducer/formReducer"
 import Label from "./Label"
 import Input from "./Input"
 import useVisibleHook from "@/customhooks/useVisibleHook"
-import { useGetAllUsersQuery } from "@/redux/services/userApi"
+import { useGetAllUsersQuery } from "@/redux/services/postApi"
 import { useAppDispatch } from "@/redux/hooks"
 import { setCredentials } from "@/redux/features/usersSlice"
 
+import { useLoginUserMutation, useRegisterUserMutation } from "@/redux/services/api"
+import { AuthProps } from "@/model/types"
+import { setCurrentUser } from "@/redux/features/usersSlice"
 
 
-export default function Signin(){
+
+export default function Signin({userExist}:AuthProps){
     const initialState = {
         email: '',
         password:''
@@ -25,35 +31,70 @@ export default function Signin(){
       const {data ,isLoading, isFetching} = useGetAllUsersQuery(null)
      
       const router = useRouter()
-    
+      const [loginUser,{isLoading:isLoadingUser, isSuccess:isSuccessLogin, data:loginResponse}] = useLoginUserMutation()
+      const [registerUser,{isLoading:isRegisteringUser, isSuccess:isSuccessRegister}] = useRegisterUserMutation()
       const handleInputChange= (event:any)=>{
         const {name, value} = event.target
         setError('')
         dispatch({
-          type: ActionTypes.textInput,
+          type: ActionTypes.siginText,
           payload: { key: [name], value: value },
         })
       }
       const dispatchUser = useAppDispatch()
       const {email, password} = state
      
-    const handleSubmit = (e:React.SyntheticEvent) =>{
+    const handleSubmit = async (e:React.SyntheticEvent) =>{
         e.preventDefault()
         if(!password || !email){
           setError('Please submit all details')
-        }
-        if(data && data.length > 0){
-          const currentLoggedInUser = data?.find(user => user.email === email)
-          if(currentLoggedInUser){
-            dispatchUser(setCredentials(currentLoggedInUser))
-            router.push('/');
-          }else{
-            setError('Please submit correct credentials')
+        } 
+        
+        const payload ={email, password}
+        if(!userExist){
+          const response:any = await registerUser(payload)
+          console.log(response.data)
+          if(response.data){
+            toast.info('Registration successful')
+          }
+          if(response?.error){
+            setError(response?.error?.data?.message)
+        
+          }
+        }else{
+          const response:any = await loginUser(payload)
+          if(response.data){
+            toast.info('Login successful')
+          }
+          if(response?.error){
+            setError(response?.error?.data?.message)
           }
         }
         
+        
+        // if(data && data.length > 0){
+        //   const currentLoggedInUser = data?.find(user => user.email === email)
+        //   if(currentLoggedInUser){
+        //     dispatchUser(setCredentials(currentLoggedInUser))
+        //     router.push('/');
+        //   }else{
+        //     setError('Please submit correct credentials')
+        //   }
+        // }
+        
 
     }
+    
+    useEffect(()=>{
+      if(isSuccessRegister){
+
+        toast.info("User registered successfully");
+      }
+      if(isSuccessLogin){
+
+      }
+    },[isSuccessRegister, isSuccessLogin])
+    console.log(loginResponse,'login')
     return (
         <div className='w-full md:w-[50%] shadow-lg rounded-2xl flex m-auto bg-white px-8 py-3 items-center justify-center h-auto mt-12'>
         <form className='w-full' onSubmit={handleSubmit}>
@@ -76,7 +117,14 @@ export default function Signin(){
           </div>
          
           <div className="my-4">
-           <button>Sign in</button>
+
+           <button>{userExist? "Sign in":"SIgn up"}</button>
+          </div>
+          <div className="my-4">
+            {userExist ? <p>Don't have an account yet? <Link href={"/auth/register"}>Sign up</Link></p>:
+            <p>Have an account? <Link href={"/auth/signin"}>Sign in</Link></p>
+            }
+           
           </div>
         </form>
       </div>
