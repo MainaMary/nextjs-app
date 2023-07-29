d27b0ac1ed79da64b33d2bc50dc47a912fc6aae4
Mary <marieshix98@gmail.com>
2023-07-29 16:45:05 +0300

    fix local storage bug

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -27,8 +27,7 @@ export default function NavBar() {
   useEffect(() => {
     setCurrentUser(localStorage.getItem(userDetailsKey) as unknown as User);
   }, [currentUser]);
- console.log(Object.keys(currentUser).length > 0)
- console.log(currentUser.name,'user name')
+ 
   return (
     <nav className=" top-0 left-0fixed flex w-full h-20 items-center px-12 bg-slate-500 justify-between">
       <Link href="/" className="text-white font-bold">
@@ -42,15 +41,18 @@ export default function NavBar() {
         ))}
       </ul>
       <div>
-        {Object.keys(currentUser).length > 0 ? (
+      <Link href={"/auth/signin"}>
+            <button>Sign in</button>
+          </Link>
+        {/* {Object?.keys(currentUser).length > 0 ? (
           <div>
             <p className="text-color-400">{currentUser.name}</p>
           </div>
         ) : (
-          <Link href={"/auth"}>
+          <Link href={"/auth/signin"}>
             <button>Sign in</button>
           </Link>
-        )}
+        )} */}
       </div>
     </nav>
   );
