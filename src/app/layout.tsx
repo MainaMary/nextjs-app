ba8f9bcb6578243c39a42e19d12311a7ea4b128c
Mary <marieshix98@gmail.com>
2023-07-31 23:55:34 +0300

    import react toastify css style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import AppStoreProvider from "../redux/services/provider";
 import NavBar from "@/components/Navbar";
 import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const inter = Inter({ subsets: ["latin"] });
 
