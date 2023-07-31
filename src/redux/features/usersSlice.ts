70b78fb420627dae78447642d0c3ca7e06fd832d
Mary <marieshix98@gmail.com>
2023-07-31 23:58:17 +0300

    code formatting

--- src/redux/features/usersSlice.ts
+++ src/redux/features/usersSlice.ts
@@ -1,75 +1,70 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { User } from "@/model/types";
-export const userDetailsKey ="userDetailsKey"
-interface currentUserType{
-    message:string, token:string
+export const userDetailsKey = "userDetailsKey";
+interface currentUserType {
+  message: string;
+  token: string;
 }
 export interface AuthState {
-    user: User,
-    signedUser: currentUserType
-   
-
+  user: User;
+  signedUser: currentUserType;
 }
-export const initialUserState =  {
-    id: '',
-    name: '',
-    username: '',
-    email:'',
-    address: {
-        street: '',
-        suite: '',
-        city: '',
-        zipcode: '',
-        geo: {
-            lat: '',
-            lng: ''
-        }
+export const initialUserState = {
+  id: "",
+  name: "",
+  username: "",
+  email: "",
+  address: {
+    street: "",
+    suite: "",
+    city: "",
+    zipcode: "",
+    geo: {
+      lat: "",
+      lng: "",
     },
-    phone: '',
-    website: '',
-    company: {
-        name: '',
-        catchPhrase: '',
-        bs: ''
-    }
-
-}
+  },
+  phone: "",
+  website: "",
+  company: {
+    name: "",
+    catchPhrase: "",
+    bs: "",
+  },
+};
 
 const initialState = {
-    // user: localStorage.getItem(userDetailsKey) ? JSON.parse(localStorage.getItem(userDetailsKey) || '')
-    // : initialUserState
-    user:initialUserState,
-    signedUser:{
-        message:"",
-        token:""
-    }
-   
-}
+  // user: localStorage.getItem(userDetailsKey) ? JSON.parse(localStorage.getItem(userDetailsKey) || '')
+  // : initialUserState
+  user: initialUserState,
+  signedUser: {
+    message: "",
+    token: "",
+  },
+};
 
 export const AuthSlice = createSlice({
-        name: 'auth',
-        initialState,
-        reducers: {
-            setCredentials :(state:AuthState,action:PayloadAction<User>) =>{
-                
-                state.user = action.payload
-               
-                
-    
-            },
-            setCurrentUser:(state:AuthState,action:PayloadAction<currentUserType>)=>{
-             state.signedUser = action.payload
-             localStorage.setItem(userDetailsKey, JSON.stringify(state.signedUser.token))
-            },
-            logOut : (state, action) =>{
-                state.user= state.user
-            }
-        }
-    })
-    export const {
-        setCredentials,
-        logOut,
-        setCurrentUser
-    } =  AuthSlice.actions;
-    const authReducer = AuthSlice.reducer
-    export { authReducer }
+  name: "auth",
+  initialState,
+  reducers: {
+    setCredentials: (state: AuthState, action: PayloadAction<User>) => {
+      state.user = action.payload;
+    },
+    setCurrentUser: (
+      state: AuthState,
+      action: PayloadAction<currentUserType>
+    ) => {
+      state.signedUser = action.payload;
+      localStorage.setItem(
+        userDetailsKey,
+        JSON.stringify(state.signedUser.token)
+      );
+    },
+    logOut: (state, action) => {
+      state.user = state.user;
+    },
+  },
+});
+export const { setCredentials, logOut, setCurrentUser } = AuthSlice.actions;
+const authReducer = AuthSlice.reducer;
+export { authReducer };
