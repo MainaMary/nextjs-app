315a6bee0dde3e2c6a46df5d311dd676e5c68247
Mary <marieshix98@gmail.com>
2023-08-01 23:43:25 +0300

    handle comment submission

--- src/components/FeedCard.tsx
+++ src/components/FeedCard.tsx
@@ -1,33 +1,61 @@
 import { useState } from "react";
 import { FaCommentAlt } from "react-icons/fa";
 import { SinglePostProps } from "@/model/types";
-import useModal from "@/customhooks/useModal";
+import Input from "./Input";
+import Button from "./Button";
 import { useAppDispatch } from "@/redux/hooks";
 import { setPostId, setEdit } from "@/redux/features/postSlice";
 import ConfirmModal from "./ConfirmModal";
 import AddPost from "./AddPost";
 import { useAppSelector } from "@/redux/hooks";
+import Modal from "./Modal";
+import { useAddCommentMutation, useGetAllPostsCommentsQuery } from "@/redux/services/api";
 interface Props {
   post: SinglePostProps;
 }
 export default function FeedCard({ post }: Props) {
-const [showModal, setShowModal] = useState<boolean>(false)
+  const [showModal, setShowModal] = useState<boolean>(false);
+  const [isComment, setIsComment] = useState<boolean>(false);
+  const [comment, setComment] = useState<string>("");
+  const [id, setId] = useState<string>("");
   const dispatch = useAppDispatch();
-  const {isEdit} = useAppSelector(state=> state.post)
-  const handleModal =() =>{
-    setShowModal(prev =>!prev)
-  }
+  const [addComment,{data, isLoading:isLoadingComment}] = useAddCommentMutation()
+  const {data:allComments} =useGetAllPostsCommentsQuery(id)
+  const { isEdit } = useAppSelector((state) => state.post);
+  const handleModal = () => {
+    setShowModal((prev) => !prev);
+  };
   const handleUpdate = (postId: string) => {
     dispatch(setPostId(postId));
-   handleModal()
-    setEdit(false)
+    handleModal();
+    dispatch(setEdit(true));
   };
+
   const handleDelete = (postId: string) => {
-    handleModal()
+    handleModal();
     dispatch(setPostId(postId));
-    setEdit(true)
+    dispatch(setEdit(false));
   };
-  console.log(showModal)
+  const handleCommentForm = () => {
+    setIsComment((prev) => !prev);
+  };
+  const handleSubmit = async (e: React.SyntheticEvent, id: string) => {
+    e.preventDefault();
+    setId(id);
+    const payload = {
+      body: comment,
+      name:'testuser',
+      postId:id,
+      email:"test@gmail.com"
+    }
+    setComment('')
+    const response = await addComment(payload)
+    if(data?.status){
+      handleCommentForm()
+    }
+    console.log({response})
+  };
+  console.log({postdata: data})
   return (
     <>
       <div className="flex flex-col mt-12 px-3 py-4 overflow-hidden rounded-lg border bg-white">
@@ -44,12 +72,43 @@ const [showModal, setShowModal] = useState<boolean>(false)
           View post
         </p>
         <div className="flex justify-between w-full mb-2 h-auto items-center">
-          <p>5 comments</p>
-          <div className="flex h-auto items-center cursor-pointer">
+         {
+          allComments?.length > 0 &&  <p>{`${allComments?.data?.length} comments`}</p>
+         }
+          <div
+            className="flex h-auto items-center cursor-pointer"
+            onClick={() => {
+              handleCommentForm(), setId(post._id);
+            }}
+          >
             <FaCommentAlt className="text-gray-500" />
             <p className="ml-4">comment</p>
           </div>
         </div>
+        {id === post._id &&  isComment && (
+          <form
+            onSubmit={(e: any) => handleSubmit(e, post._id)}
+            className="flex"
+          >
+            <div className="my-4 block md:flex w-full justify-between">
+              {/* <CustomInput
+                        placeholder="e.g Good article"
+                        name="labelId"
+                        onChange={(e: any) => setDataComment((dataComment:any)=> ({...dataComment, [label._id]:e.target.value}))}
+                        type="text"
+                        value={dataComment[label._id]}
+                      /> */}
+              <Input
+                placeholder="e.g Good article"
+                name="comment"
+                onChange={(e: any) => setComment(e.target.value)}
+                type="text"
+                value={comment}
+              />
+              <Button className="md:ml-3">Add</Button>
+            </div>
+          </form>
+        )}
         <div className="flex h-auto items-center border-[2px] my-3 border-b border-gray-500"></div>
         <div className="flex justify-between mt-2">
           <button
@@ -66,8 +125,14 @@ const [showModal, setShowModal] = useState<boolean>(false)
           </button>
         </div>
       </div>
-      {showModal && <ConfirmModal />}
-      {showModal   && <AddPost openModal={showModal} handleModal={handleModal}/>}
+      {showModal && !isEdit && (
+        <ConfirmModal openModal={showModal} handleModal={handleModal} />
+      )}
+      {showModal && isEdit && (
+        <Modal>
+          <AddPost openModal={showModal} handleModal={handleModal} />
+        </Modal>
+      )}
     </>
   );
 }
