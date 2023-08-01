59e531c4310d91b2845822caed9eed854868be60
Mary <marieshix98@gmail.com>
2023-08-01 23:45:26 +0300

    add post reducer to the store

--- src/redux/services/store.ts
+++ src/redux/services/store.ts
@@ -2,12 +2,14 @@ import {configureStore} from "@reduxjs/toolkit"
 import { setupListeners } from "@reduxjs/toolkit/dist/query";
 import { authReducer } from "@/redux/features/usersSlice";
 import { counterReducer } from "../features/paginationSlice";
+import { postReducer } from "../features/postSlice";
 import { userApi } from "./postApi";
 import { api } from "./api";
 export const store = configureStore({
     reducer:{
       auth:authReducer,
       counter: counterReducer,
+      post: postReducer,
       [userApi.reducerPath]: userApi.reducer,
       [api.reducerPath] :api.reducer
     },
