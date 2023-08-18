54592cf6cc4f2dbda0e1295ff52c90ad383e296c
Mary <marieshix98@gmail.com>
2023-08-18 23:54:24 +0300

    edit comment modal

--- src/components/CommentForm.tsx
+++ src/components/CommentForm.tsx
@@ -1,39 +1,54 @@
-import React,{useEffect} from "react";
+import React,{useEffect, useState} from "react";
 import Input from "./Input";
 import Button from "./Button";
 import useLocalStorage from "@/customhooks/useLocalStorage";
-import { useAddCommentMutation,useGetSingleCommentQuery} from "@/redux/services/api";
+import { useAddCommentMutation,useGetSingleCommentQuery, useUpdateCommentMutation} from "@/redux/services/api";
 import { useAppSelector } from "@/redux/hooks";
-
+import { toast } from "react-toastify";
 interface Props {
   id: string;
-  comment: string;
-  setComment: any;
+ 
   handleCommentForm: () => void;
 }
-const CommentForm = ({ comment, id, setComment, handleCommentForm }: Props) => {
+const CommentForm = ({  id, handleCommentForm }: Props) => {
+  const [comment, setComment] = useState('')
   const { currentUser } = useLocalStorage();
   const [addComment, { data, isLoading: isLoadingComment }] =
     useAddCommentMutation();
+const [updateComment] = useUpdateCommentMutation()
 const {postId} = useAppSelector(state =>state.post)
 const {data:editedComment, isSuccess} = useGetSingleCommentQuery(postId)
 
 console.log(editedComment)
-// useEffect(() =>{
-//     if(isSuccess){
-//       const newObj = {
-//         title: editedComment.data.title,
-//         body: editedPost.data.body,
-//         id: editedPost.data._id,
-//       };
-//       setFormValues({...newObj})
-//     }
+useEffect(() =>{
+    if(isSuccess){
+      const newObj = {
+        body: editedComment.data.body,
+        name: "testuser",
+        postId,
+        email: editedComment.data.email,
+      };
+      setComment(newObj.body)
+    }
    
-//   },[editedComment])
+  },[editedComment, setComment])
   const handleSubmit = async (e: React.SyntheticEvent) => {
     e.preventDefault();
     if(postId){
-
+      const updatedPayload = {
+        body:comment,
+        name: "testuser",
+        postId:editedComment.data._id,
+        email: editedComment.data.email,
+        
+      }
+      const response:any = await updateComment(updatedPayload);
+      console.log(response.data.status)
+       if (response.data.status) {
+         toast.info('Comment successfully edited')
+        
+        }
+    
     }else{
         const payload = {
             body: comment,
